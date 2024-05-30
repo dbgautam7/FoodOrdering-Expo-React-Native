10535c7b07fbd5951fc6165f2ac1ad9ac6617888
dbgauatm7 <gautamdb28@gmail.com>
2024-05-30 23:25:19 +0545

    feat:expo router setup and dynamic component made

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,31 +1,37 @@
-import { Image, StyleSheet } from 'react-native';
-import { Text, View } from '@/src/components/Themed';
+import {Image, Pressable, StyleSheet} from 'react-native';
+import {Text} from '@/src/components/Themed';
 import Colors from '@/src/constants/Colors';
-import { Product } from '@/src/types';
+import {Product} from '@/src/types';
+import {Link} from 'expo-router';
 
-
-
-const ProductListItem = ({ product }: {product:Product}) => {
+const ProductListItem = ({product}: {product: Product}) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: product.image || '',
-        }}
-        style={styles.image}
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <Link href={`/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{
+            uri: product.image || '',
+          }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
-export default ProductListItem
+export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
     padding: 16,
     alignItems: 'center',
+    flex: 1,
+    borderRadius: 20,
+    maxWidth: '50%',
+    backgroundColor: Colors.light.tabIconSelected,
   },
   title: {
     fontSize: 18,
@@ -34,7 +40,7 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 14,
     fontWeight: '400',
-    color: Colors.light.tint,
+    color: Colors.light.text,
   },
   image: {
     width: '100%',
